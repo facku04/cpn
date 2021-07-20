1b00aa0abf9c755e3a02d1bb724cd034a13e82ac
facku04 <87353668+facku04@users.noreply.github.com>
2021-07-20 14:19:41 -0300

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,27 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imageneslight =  document.querySelector('.agregar-imagen')
+const contenedorlight = document.querySelector('.imagen-light')
+const hamburger1 = document.querySelector('.hambur');
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerimagen(imagen.getAttribute('src')) 
+    })
+})
+
+contenedorlight.addEventListener('click', (e)=>{
+    if(e.target !== imageneslight){
+        contenedorlight.classList.toggle('show')
+        imageneslight.classList.toggle('showimage')
+        hamburger1.style.opacity ='1'
+    }
+})
+
+
+const aparecerimagen = (imagen)=>{
+    imageneslight.src = imagen
+    contenedorlight.classList.toggle('show')
+    imageneslight.classList.toggle('showimage')
+    hamburger1.style.opacity ='0'
+}
